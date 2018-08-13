99eab8cb034a0410b17f9770b5ed2aec2e7941df
lbilde <ljb@easv.dk>
2018-08-13 12:41:39 +0200

    initial commit

---
+++ Cust.Infrastructure.Data/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using Cust.Core.DomainService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cust.Infrastructure.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private CustomerAppDBContext _context;
+
+        public ICustomerRepository CustomerRepository { get; internal set; }
+
+        public UnitOfWork(  CustomerAppDBContext context, 
+                            ICustomerRepository customerRepository)
+        {
+            this._context = context;
+            this.CustomerRepository = customerRepository;
+        }
+
+        public int Commit()
+        {
+            return this._context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            this._context.Dispose();
+        }
+    }
+}
